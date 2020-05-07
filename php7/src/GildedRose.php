aabde2f0328231794d75971ae28a3d17056e77ca
Vilius Grigaliūnas <vilius.grigaliunas@cinemaads.lt>
2020-05-07 23:14:39 +0300

    Use increment/decrement operators

--- php7/src/GildedRose.php
+++ php7/src/GildedRose.php
@@ -24,21 +24,21 @@ final class GildedRose
             if (KnownItemName::AGED_BRIE !== $item->name && KnownItemName::BACKSTAGE_PASSES !== $item->name) {
                 if ($item->quality > 0) {
                     if (KnownItemName::SULFURAS !== $item->name) {
-                        $item->quality = $item->quality - 1;
+                        --$item->quality;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    ++$item->quality;
                     if (KnownItemName::BACKSTAGE_PASSES === $item->name) {
                         if ($item->sell_in < 11) {
                             if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                                ++$item->quality;
                             }
                         }
                         if ($item->sell_in < 6) {
                             if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                                ++$item->quality;
                             }
                         }
                     }
@@ -46,7 +46,7 @@ final class GildedRose
             }
 
             if (KnownItemName::SULFURAS !== $item->name) {
-                $item->sell_in = $item->sell_in - 1;
+                --$item->sell_in;
             }
 
             if ($item->sell_in < 0) {
@@ -54,15 +54,15 @@ final class GildedRose
                     if (KnownItemName::BACKSTAGE_PASSES !== $item->name) {
                         if ($item->quality > 0) {
                             if (KnownItemName::SULFURAS !== $item->name) {
-                                $item->quality = $item->quality - 1;
+                                --$item->quality;
                             }
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality -= $item->quality;
                     }
                 } else {
                     if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                        ++$item->quality;
                     }
                 }
             }
