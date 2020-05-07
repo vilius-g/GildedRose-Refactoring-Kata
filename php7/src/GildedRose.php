6103bab8e335d27de8085780af1a016bd733fcdb
Vilius Grigaliūnas <vilius.grigaliunas@cinemaads.lt>
2020-05-07 23:13:29 +0300

    Use strict equality operators

--- php7/src/GildedRose.php
+++ php7/src/GildedRose.php
@@ -21,16 +21,16 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if (KnownItemName::AGED_BRIE != $item->name and KnownItemName::BACKSTAGE_PASSES != $item->name) {
+            if (KnownItemName::AGED_BRIE !== $item->name && KnownItemName::BACKSTAGE_PASSES !== $item->name) {
                 if ($item->quality > 0) {
-                    if (KnownItemName::SULFURAS != $item->name) {
+                    if (KnownItemName::SULFURAS !== $item->name) {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if (KnownItemName::BACKSTAGE_PASSES == $item->name) {
+                    if (KnownItemName::BACKSTAGE_PASSES === $item->name) {
                         if ($item->sell_in < 11) {
                             if ($item->quality < 50) {
                                 $item->quality = $item->quality + 1;
@@ -45,15 +45,15 @@ final class GildedRose
                 }
             }
 
-            if (KnownItemName::SULFURAS != $item->name) {
+            if (KnownItemName::SULFURAS !== $item->name) {
                 $item->sell_in = $item->sell_in - 1;
             }
 
             if ($item->sell_in < 0) {
-                if (KnownItemName::AGED_BRIE != $item->name) {
-                    if (KnownItemName::BACKSTAGE_PASSES != $item->name) {
+                if (KnownItemName::AGED_BRIE !== $item->name) {
+                    if (KnownItemName::BACKSTAGE_PASSES !== $item->name) {
                         if ($item->quality > 0) {
-                            if (KnownItemName::SULFURAS != $item->name) {
+                            if (KnownItemName::SULFURAS !== $item->name) {
                                 $item->quality = $item->quality - 1;
                             }
                         }
