934ddde68df92803436858c6b7046cfb323cd6c8
Vilius Grigaliūnas <vilius.grigaliunas@cinemaads.lt>
2020-05-07 22:15:14 +0300

    Fixed broken test

--- php7/tests/GildedRoseTest.php
+++ php7/tests/GildedRoseTest.php
@@ -4,14 +4,17 @@ namespace App\Tests;
 
 use App\GildedRose;
 use App\Item;
+use PHPUnit\Framework\TestCase;
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase
+class GildedRoseTest extends TestCase
 {
-    public function testFoo()
+    public function testFoo(): void
     {
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals('fixme', $items[0]->name);
+        $this->assertEquals('foo', $items[0]->name);
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(0, $items[0]->quality);
     }
 }
