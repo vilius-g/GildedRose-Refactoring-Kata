8dc5e3b8d71356cf47ec9dde4925e09623dc0c93
Vilius Grigaliūnas <vilius.grigaliunas@cinemaads.lt>
2020-05-07 22:12:05 +0300

    Run texttest test using phpunit

---
+++ php7/tests/TextTest/GildedRoseTextTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\TextTest;
+
+use PHPUnit\Framework\TestCase;
+use function file_get_contents;
+use function ob_get_clean;
+use function ob_start;
+
+class GildedRoseTextTest extends TestCase
+{
+    public function testUsingTextTest(): void
+    {
+        $expectedContent = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'GildedRoseTextTest.txt');
+        $actualContent = $this->runTextTestFixture();
+        self::assertEquals($expectedContent, $actualContent);
+    }
+
+    /**
+     * Run fixture file and return its output.
+     *
+     * @return string
+     */
+    private function runTextTestFixture(): string
+    {
+        ob_start();
+        $argv = ['texttest_fixture.php', '11'];
+        include __DIR__.'/../../fixtures/texttest_fixture.php';
+
+        return ob_get_clean();
+    }
+}
